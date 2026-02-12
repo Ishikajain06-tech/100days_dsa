7803744de4e5e2247ea92c7738d1d1872a430cac
ishika jain <ishika.22055@stu.upes.ac.in>
2026-02-12 14:50:05 +0530

    Implement string reversal function

---
+++ dsa_day09q2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void reverseString(char* s, int size) {
+    int left = 0, right = size - 1;
+
+    while (left < right) {
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        left++;
+        right--;
+    }
+}
