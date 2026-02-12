93180724ec5c584f858bfea31de73040d7e68718
ishika jain <ishika.22055@stu.upes.ac.in>
2026-02-12 14:06:57 +0530

    Add maxProfit function for stock price analysis
    Implement maxProfit function to calculate maximum profit from stock prices.

---
+++ dsa_day2 Q2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int maxProfit(int prices[], int n) {
+    int minPrice = prices[0];
+    int maxProfit = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        }
+        else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+    return maxProfit;
+}
