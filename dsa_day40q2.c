3d361ffcb0ae945d41bcdd54b03fed6ebe518be2
ishika jain <ishika.22055@stu.upes.ac.in>
2026-03-12 19:52:50 +0530

    Add dailyTemperatures function for temperature analysis
    Implement dailyTemperatures function to calculate days until warmer temperatures.

---
+++ dsa_day40q2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* dailyTemperatures(int* temperatures, int temperaturesSize, int* returnSize) {
+    
+    int *result = (int*)calloc(temperaturesSize, sizeof(int));
+    int *stack = (int*)malloc(sizeof(int) * temperaturesSize);
+    int top = -1;
+
+    for(int i = 0; i < temperaturesSize; i++) {
+        
+        while(top >= 0 && temperatures[i] > temperatures[stack[top]]) {
+            int index = stack[top--];
+            result[index] = i - index;
+        }
+
+        stack[++top] = i;
+    }
+
+    *returnSize = temperaturesSize;
+    free(stack);
+    return result;
+}
