02b9da39c29802bef265c52a0dffc83cd92aa02e
ishika jain <ishika.22055@stu.upes.ac.in>
2026-03-24 08:16:12 +0530

    Create dsa_day49q2.c

---
+++ dsa_day49q2.c
@@ -0,0 +1,19 @@
+struct TreeNode* insertIntoBST(struct TreeNode* root, int val) {
+    // If tree is empty, create a new node
+    if (root == NULL) {
+        struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+        newNode->val = val;
+        newNode->left = NULL;
+        newNode->right = NULL;
+        return newNode;
+    }
+
+    // Traverse to the correct position
+    if (val < root->val) {
+        root->left = insertIntoBST(root->left, val);
+    } else {
+        root->right = insertIntoBST(root->right, val);
+    }
+
+    return root; // return unchanged root
+}
