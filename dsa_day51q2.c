74755281370c603d34e9145fdd7ce1be0ecdace6
ishika jain <ishika.22055@stu.upes.ac.in>
2026-03-24 08:20:29 +0530

    Create dsa_day51q2.c

---
+++ dsa_day51q2.c
@@ -0,0 +1,16 @@
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    while (root != NULL) {
+        // Both nodes in left subtree
+        if (p->val < root->val && q->val < root->val)
+            root = root->left;
+
+        // Both nodes in right subtree
+        else if (p->val > root->val && q->val > root->val)
+            root = root->right;
+
+        // Split point → this is LCA
+        else
+            return root;
+    }
+    return NULL;
+}
