fee83bc5e50836a8829964470c885713e5bf14ec
ishika jain <ishika.22055@stu.upes.ac.in>
2026-04-25 10:01:46 +0530

    Implement binary search to find peak element

---
+++ dsa_day84q2.c
@@ -0,0 +1,18 @@
+int findPeakElement(int* nums, int numsSize) {
+    int left = 0;
+    int right = numsSize - 1;
+
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] > nums[mid + 1]) {
+            // Peak is in left half (including mid)
+            right = mid;
+        } else {
+            // Peak is in right half
+            left = mid + 1;
+        }
+    }
+
+    return left;  // or right (both are same)
+}
