c66c7d5ed5172f50d3a6fd88a721a75700118c46
ishika jain <ishika.22055@stu.upes.ac.in>
2026-05-02 11:27:40 +0530

    Implement Dutch National Flag algorithm for sorting

---
+++ dsa_day91q2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// Swap function
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void sortColors(int* nums, int numsSize) {
+    int low = 0, mid = 0, high = numsSize - 1;
+
+    while (mid <= high) {
+        if (nums[mid] == 0) {
+            swap(&nums[low], &nums[mid]);
+            low++;
+            mid++;
+        } 
+        else if (nums[mid] == 1) {
+            mid++;
+        } 
+        else { // nums[mid] == 2
+            swap(&nums[mid], &nums[high]);
+            high--;
+        }
+    }
+}
